2938a6cd7eee05f2ba990a30a317ffacf98c2101
michael <sargsyanmikael@mail.ru>
2022-10-23 20:59:38 +0300

    add imgs

---
+++ src/assets/imgs/index.ts
@@ -0,0 +1,39 @@
+import { ReactComponent as LogoIcon } from './icons/logo-company.svg';
+import { ReactComponent as PhoneIcon } from './icons/phone.svg';
+import ContentImage1 from './contentImgs/julia-v1.png';
+import ContentImage2 from './contentImgs/taisiia-v1.png';
+import ContentImage3 from './contentImgs/julia-v2.png';
+import ContentImage4 from './contentImgs/taisiia-v2.png';
+import ContentMobileImage1 from './contentMobileImgs/julia-v1-mobile.jpg';
+import ContentMobileImage2 from './contentMobileImgs/taisia-v1-mobile.jpg';
+import ContentMobileImage3 from './contentMobileImgs/julia-v2-mobile.jpg';
+import ContentMobileImage4 from './contentMobileImgs/taisia-v2-mobile.jpg';
+import Gallery1 from './contentImgs/slider/gallery-1.jpg';
+import Gallery2 from './contentImgs/slider/gallery-2.jpg';
+import Gallery3 from './contentImgs/slider/gallery-3.jpg';
+import Gallery4 from './contentImgs/slider/gallery-4.jpg';
+import Gallery5 from './contentImgs/slider/gallery-5.jpg';
+import Gallery6 from './contentImgs/slider/gallery-6.jpg';
+import Gallery7 from './contentImgs/slider/gallery-7.jpg';
+import Gallery8 from './contentImgs/slider/gallery-8.jpg';
+
+export {
+    LogoIcon,
+    PhoneIcon,
+    ContentImage1,
+    ContentImage2,
+    ContentImage3,
+    ContentImage4,
+    ContentMobileImage1,
+    ContentMobileImage2,
+    ContentMobileImage3,
+    ContentMobileImage4,
+    Gallery1,
+    Gallery2,
+    Gallery3,
+    Gallery4,
+    Gallery5,
+    Gallery6,
+    Gallery7,
+    Gallery8,
+};
