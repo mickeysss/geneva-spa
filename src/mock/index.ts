c0736df95a72bed05919da2e86a772b483383d30
michael <sargsyanmikael@mail.ru>
2022-10-24 17:54:23 +0300

    changed mock data

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,13 +1,4 @@
-import {
-    ContentImage1,
-    ContentImage2,
-    ContentImage3,
-    ContentImage4,
-    ContentMobileImage1,
-    ContentMobileImage2,
-    ContentMobileImage3,
-    ContentMobileImage4,
-} from 'assets/imgs';
+import { ContentImage1, ContentImage2, ContentImage3, ContentImage4 } from 'assets/imgs';
 
 export const data = [
     {
@@ -17,7 +8,6 @@ export const data = [
             {
                 id: 1.1,
                 contentImage: ContentImage1,
-                contentMobileImage: ContentMobileImage1,
                 contentMainText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                 contentAdditionalText: [
                     'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n',
@@ -27,7 +17,6 @@ export const data = [
             {
                 id: 1.2,
                 contentImage: ContentImage2,
-                contentMobileImage: ContentMobileImage2,
                 contentMainText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                 contentAdditionalText: [
                     'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n',
@@ -43,7 +32,6 @@ export const data = [
             {
                 id: 2.1,
                 contentImage: ContentImage3,
-                contentMobileImage: ContentMobileImage3,
                 contentMainText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                 contentAdditionalText: [
                     'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n',
@@ -53,7 +41,6 @@ export const data = [
             {
                 id: 2.2,
                 contentImage: ContentImage4,
-                contentMobileImage: ContentMobileImage4,
                 contentMainText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
                 contentAdditionalText: [
                     'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n',
