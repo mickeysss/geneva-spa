06a6dc3f62d3e0063ad28b2b957f75660f90226f
michael <sargsyanmikael@mail.ru>
2022-10-24 17:53:35 +0300

    fix style aligning

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -2,26 +2,25 @@ import React from 'react';
 import { Header, Footer } from 'components';
 import { ContentPage } from 'pages/ContentPage';
 import { GalleryPage } from 'pages/GalleryPage';
+import { RESPONSIVE_SIZES } from 'consts';
 import { useMediaQuery } from 'utils/useMediaQuery';
 import { LogoIcon, PhoneIcon } from 'assets/imgs';
-import { RESPONSIVE_SIZES } from 'consts';
 import { data } from 'mock';
-import styles from './index.module.css';
 
 const pageOne = Object.assign({}, ...data.filter((page) => page.pageId === 1));
 const pageTwo = Object.assign({}, ...data.filter((page) => page.pageId === 2));
 
 export const Layout = (): JSX.Element => {
-    const matches = (size: number) => useMediaQuery(`(max-width: ${size}px)`);
+    const TABLET_DEVICE = useMediaQuery(`(max-width: ${RESPONSIVE_SIZES.TABLET}px)`);
 
     return (
         <>
             <Header>
                 <LogoIcon />
-                {!matches(RESPONSIVE_SIZES.TABLET) ? (
+                {!TABLET_DEVICE ? (
                     <a href="tel:+7 (495) 495-49-54">+7 (495) 495-49-54</a>
                 ) : (
-                    <PhoneIcon className={styles.phoneLogo} />
+                    <PhoneIcon />
                 )}
             </Header>
             <ContentPage item={pageOne} />
